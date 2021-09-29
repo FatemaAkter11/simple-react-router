b2205bd59f91d3de804f1910584024ae0092bb52
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-29 13:11:15 +0600

    Create route with some fixed and some dynamic part

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Friend = (props) => {
-    const { name, phone, website, address } = props.friend;
+    const { id, name, phone, website, address } = props.friend;
     const FriendSyles = {
         border: '2px solid greenyellow',
         borderRadius: '10px',
         padding: '10px'
     }
+    const url = `/friend/${id}`;
     return (
         <div style={FriendSyles}>
-            <h2>This is {name}</h2>
+            <h2>This is {name} {id}</h2>
             <h5>Call me:{phone}</h5>
             <h6>Visit me:{website}</h6>
             <p><small>I live in {address.city}</small></p>
+            <Link to={url}>Please Visit Me</Link>
         </div>
     );
 };
