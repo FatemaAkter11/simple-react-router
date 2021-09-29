7cca610a9940864ace564cdb855ce5af52ff7033
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-29 19:33:55 +0600

    dynamic route using a button click

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 const Friend = (props) => {
     const { id, name, phone, website, address } = props.friend;
+
+    const history = useHistory();
+
     const FriendSyles = {
         border: '2px solid greenyellow',
         borderRadius: '10px',
         padding: '10px'
     }
+    const btnStyle = {
+        backgroundColor: 'tomato',
+        color: 'white',
+        fontSize: '20px',
+        border: '1px solid lightgray',
+        borderRadius: '5px',
+        padding: '10px'
+    }
+
     const url = `/friend/${id}`;
+
+    const handleFriendClick = () => {
+        history.push(`/friend/${id}`);
+    }
+
     return (
         <div style={FriendSyles}>
             <h2>This is {name} {id}</h2>
@@ -16,6 +33,14 @@ const Friend = (props) => {
             <h6>Visit me:{website}</h6>
             <p><small>I live in {address.city}</small></p>
             <Link to={url}>Please Visit Me</Link>
+            <br />
+            <br />
+            <Link to={url}>
+                <button style={btnStyle}>Visit Me</button>
+            </Link>
+            <br />
+            <br />
+            <button style={btnStyle} onClick={handleFriendClick}>Visit me 2</button>
         </div>
     );
 };
--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 const FriendDetail = () => {
     const { friendId } = useParams();
     const [friend, setFriend] = useState({});
+
+    const history = useHistory();
 
     useEffect(() => {
         const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
@@ -17,13 +19,27 @@ const FriendDetail = () => {
         padding: '10px',
         margin: '150px'
     }
+    const btnStyle = {
+        backgroundColor: 'tomato',
+        color: 'white',
+        fontSize: '20px',
+        border: '1px solid lightgray',
+        borderRadius: '5px',
+        padding: '10px'
+    }
+
+    const handleClick = () => {
+        history.push('/friends');
+    }
+
     return (
         <div style={FriendSyles}>
             <h3>Friend detail is coming soon: {friendId}</h3>
-            <h4>This is: {friend.name}</h4>
-            <h5>Call me: {friend.phone}</h5>
+            <h2>This is: {friend.name}</h2>
+            <h3>Call me: {friend.phone}</h3>
             <h5>Visit Me: {friend.website}</h5>
-            <h6>Company:{friend.company?.name} </h6>
+            <h4>Works At:{friend.company?.name} </h4>
+            <button style={btnStyle} onClick={handleClick}>All the Friends</button>
         </div>
     );
 };
